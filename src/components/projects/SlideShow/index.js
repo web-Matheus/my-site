2dd35a2851a522ba415cfcb4280ee3d0b7e6520a
web-Matheus <matheussantosjere02@gmail.com>
2022-04-18 09:48:31 -0300

    ainda trabalhando na aba projects

--- src/components/projects/SlideShow/index.js
+++ src/components/projects/SlideShow/index.js
@@ -1,48 +1,22 @@
-import {Slide} from 'react-slideshow-image'
-import 'react-slideshow-image/dist/styles.css'
 import './index.scss'
 import img1 from './img/imagem.png'
 import img2 from './img/codingame.png'
 
-const SlideShow = ()=>{
-    const slideImages =[
-       img1,
-       img2
-      ]
 
-      const properties = {
-        duration: 5000,
-        transitionDuration: 500,
-        infinite: true,
-        indicators: true,
-        arrows: true
-      }
-    
+
+const SlideShow = ()=>{
+
     return(
-      <div className='cards'>
-      <Slide {...properties}>
-      <div className="each-slide">
-        <div style={{'backgroundImage': `url(${slideImages[0]})`}} className="card-front">
-          <div className='card-back'>
-          <span>Slide 1</span>
-          <p>esse paragrafo é para ter display none q quando o mouse hover o e elemento aparecer</p>
-          </div>
+      <div className='body'>
+           <div class="card">
+            <img src={img1} alt="Avatar"/>
+            <div className='container'>
+              <h4><b>Caderno de anotações</b></h4> 
+              <p>Uma aplicação fullstack com NODE e REACT </p> 
+            </div>
+          </div> 
         </div>
-      </div>
-      <div className="each-slide">
-        <div style={{'backgroundImage': `url(${slideImages[1]})`}}>
-          <span>Slide 2</span>
-          <p>esse paragrafo é para ter display none q quando o mouse hover o e elemento aparecer</p>
-        </div>
-      </div>
-      <div className="each-slide">
-        <div style={{'backgroundImage': `url(${slideImages[2]})`}}>
-          <span>Slide 3</span>
-          <p>esse paragrafo é para ter display none q quando o mouse hover o e elemento aparecer</p>
-        </div>
-      </div>
-    </Slide>
-    </div>)
+    )
   }
 
   export default SlideShow;
