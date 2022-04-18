4c4db18877c725c49835157ccb517e8cdee407cb
web-Matheus <matheussantosjere02@gmail.com>
2022-04-18 15:58:06 -0300

    primeira versão do projeto pronto para ser publicada

--- src/components/projects/SlideShow/index.js
+++ src/components/projects/SlideShow/index.js
@@ -1,20 +1,21 @@
 import './index.scss'
 import img1 from './img/imagem.png'
-import img2 from './img/codingame.png'
+
 
 
 
 const SlideShow = ()=>{
-
+    
     return(
       <div className='body'>
            <div class="card">
             <img src={img1} alt="Avatar"/>
             <div className='container'>
-              <h4><b>Caderno de anotações</b></h4> 
-              <p>Uma aplicação fullstack com NODE e REACT </p> 
+              <h4><b>Projetos em desenvolvimento</b></h4> 
+              <p>Você pode verificar alguns dos meus projetos no github, lá você tem acesso ao código fonte como tambem no read.Md haverá um link para ver o projeto funcionando</p> 
+              <a href='https://github.com/web-Matheus?tab=repositories'>Ver código</a>
             </div>
-          </div> 
+        </div>
         </div>
     )
   }
